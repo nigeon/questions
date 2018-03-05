9e466cb7bf736c0199a8254330c207d4808575d2
nigeon <nigeon@gmail.com>
2018-03-05 17:15:12 +0100

    Moved to Drizzle box...

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,11 @@
 var QuestionFactory = artifacts.require("./QuestionFactory.sol");
+var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var TutorialToken = artifacts.require("./TutorialToken.sol");
+var ComplexStorage = artifacts.require("./ComplexStorage.sol");
+
 module.exports = function(deployer) {
   deployer.deploy(QuestionFactory);
+  deployer.deploy(SimpleStorage);
+  deployer.deploy(TutorialToken);
+  deployer.deploy(ComplexStorage);
 };
